fa8da81f7ba45f007b61bf1b5e264c61bf91be7d
LuisUNI21 <lopezvadoluis789@gmail.com>
2025-04-13 22:55:48 -0600

    tercero terminado

--- TercerDiagrama.py
+++ TercerDiagrama.py
@@ -12,7 +12,7 @@ df_pen_sales["Tiempo de entrega"] = tiempo_de_entrega
 tiempo_medio_de_entrega = df_pen_sales.groupby("Item")["Tiempo de entrega"].mean().sort_values()
 plt.figure(figsize=(10, 5))
 tiempo_medio_de_entrega.plot(kind="bar", color="pink")
-plt.title("Tiempo medio de entrega de productos")
+plt.title("Tiempo medio de entrega de producto")
 plt.xlabel("Tipo de producto")
 plt.ylabel("Tiempo medio de entrega")
 plt.xticks(rotation=45, ha='right')
